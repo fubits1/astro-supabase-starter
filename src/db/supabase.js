89fa3bf50e5af426345d08d2a8606bbebc594112
fubits1 <ilja_s@protonmail.com>
2024-03-16 22:00:44 +0100

    build: try env workaround

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,6 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+const SUPABASE_URL = import.meta.env.MODE === 'development' ? import.meta.env.SUPABASE_URL : process.env.SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.MODE === 'development' ? import.meta.env.SUPABASE_ANON_KEY : process.env.SUPABASE_ANON_KEY;
+
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
 );
